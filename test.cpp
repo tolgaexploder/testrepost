9e4c158056150146f0b0e3fea91d65dd3d6784c3
tolgaexploder <71831052+tolgaexploder@users.noreply.github.com>
2021-12-22 18:24:59 +0300

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,68 @@
+INT64(__fastcall* Qword_ptrOriginal)(PVOID, PVOID, PVOID, PVOID, PVOID);
+
+INT64 __fastcall NtExample(PVOID a1, PVOID a2, PVOID SectionInfo, PVOID a4, PVOID a5)
+{
+
+	return Qword_ptrOriginal(a1, a2, SectionInfo, a4, a5);
+}
+
+extern "C" NTSTATUS Main(PDRIVER_OBJECT DriverObj, PUNICODE_STRING RegistryPath)
+{
+	UNREFERENCED_PARAMETER(DriverObj);
+	UNREFERENCED_PARAMETER(RegistryPath);
+
+
+	auto wink32base = mem::GetSystemBaseModule("\\SystemRoot\\System32\\win32kbase.sys");
+
+	if (wink32base)
+	{
+		auto dataPtr = FindPattern((UINT64)wink32base, (UINT64)0xFFFFFFFFFF, (BYTE*)"\x74\x20\x48\x8B\x44\x24\x00\x44", "xxxxxx?x");
+
+		if (dataPtr)
+		{
+			UINT64 qword_deref = (UINT64)(dataPtr);
+
+			qword_deref = (UINT64)qword_ptr_derf + *(PINT)((PBYTE)qword_ptr_derf + 3) + 7; //6
+
+			auto RVA = qword_ptr_derf - (UINT64)wink32base;
+
+			PEPROCESS Target;
+			NTSTATUS Status;
+
+			if (NT_SUCCESS(Status = FindProcess("winloon.exe", &Target)))
+			{
+				if (Target)
+				{
+					KeAttachProcess(Target);
+
+					*(PVOID*)&Qword_ptrOriginal = InterlockedExchangePointer((PVOID*)qword_ptr_derf, (PVOID)NtSetCompositionSurfaceAnalogExclusive);
+
+					KeDetachProcess();
+				}
+				else
+				{
+					Printf("Error!");
+				}
+			}
+			else
+			{
+				Printf("Error! explorer.exe not found! Status : 0x%x", Status);
+			}
+
+
+		}
+		else
+		{
+			Printf("Error!\n");
+		}
+	}
+	else
+	{
+		Printf("Error!\n");
+
+	}
+
+
+	Printf("hook\n");
+	return STATUS_SUCCESS;
+}
